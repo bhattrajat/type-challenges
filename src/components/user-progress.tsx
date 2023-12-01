4c5afd4c159bd10a13cd165e97182b8c86f31afc
Rajat Bhatt <rajatbhatt999@gmail.com>
2023-11-30 20:40:53 -0500

    Add github repo url

--- src/components/user-progress.tsx
+++ src/components/user-progress.tsx
@@ -27,12 +27,12 @@ export default function UserProgress(props: Props) {
   }
   const difficulties = ["easy", "medium", "hard", "extreme"] as const;
   return (
-    <div className="mb-4 lg:grid lg:grid-cols-4 lg:gap-4">
+    <div className="mb-4 grid grid-cols-2 lg:grid-cols-4 gap-4">
       {difficulties.map((difficulty) => (
         <div key={difficulty}>
           <div>{`${difficulty[0].toUpperCase()}${difficulty.slice(1)}`}</div>
           <progress
-            className=" [&::-webkit-progress-value]:transition-[width] [&::--moz-progress-bar]:transition-[width] [&::-webkit-progress-value]:bg-blue-700 [&::-moz-progress-bar]:bg-blue-700"
+            className="[&::-webkit-progress-value]:transition-[width] [&::--moz-progress-bar]:transition-[width] [&::-webkit-progress-value]:bg-blue-700 [&::-moz-progress-bar]:bg-blue-700 [&::-webkit-progress-value]:duration-[.5s]"
             id="easy"
             max={totalCount.easy}
             value={completedCount[difficulty]}
